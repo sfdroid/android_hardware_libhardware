39afbbefd03632c347e38c37e4d34c112f097168
RealJohnGalt <johngaltfirstrun@gmail.com>
2016-07-03 18:00:05 -0700

    modules: treat sfdroid modules normally
    Change-Id: Ifb9bc132e4b6c75389e31623ab961f7f01a3896d

--- modules/Android.mk
+++ modules/Android.mk
@@ -1,4 +1,4 @@
 hardware_modules := gralloc hwcomposer audio nfc nfc-nci local_time \
 	power usbaudio audio_remote_submix camera consumerir sensors vibrator \
-	tv_input fingerprint
+	tv_input fingerprint sfdroid_sensors sharebuffer
 include $(call all-named-subdir-makefiles,$(hardware_modules))
--- modules/sfdroid_sensors/Android.mk
+++ modules/sfdroid_sensors/Android.mk
@@ -19,12 +19,11 @@ LOCAL_PATH := $(call my-dir)
 # hw/<SENSORS_HARDWARE_MODULE_ID>.<ro.hardware>.so
 include $(CLEAR_VARS)
 
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE := sfdroid_sensors.default
+LOCAL_MODULE_RELATIVE_PATH := hw
+
 LOCAL_SHARED_LIBRARIES := liblog libcutils
 LOCAL_SRC_FILES := sfdroid_sensors.c
-ifeq ($(TARGET_PRODUCT),vbox_x86)
-LOCAL_MODULE := sfdroid_sensors.vbox_x86
-else
-LOCAL_MODULE := sfdroid_sensors.default
-endif
+
+LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
